1d0fd795a3f962d0ab7388328e6cf4089bdfbf77
Allan Elsberry <37273149+ender18g@users.noreply.github.com>
2022-11-15 09:33:41 -0500

    day 2 - added score, clock, border, mousemotion

--- game.py
+++ game.py
@@ -1,25 +1,18 @@
 import pygame
 from time import sleep
+from helper import grid
+import sys
+from tank import Tank
 
 # init pygame
 pygame.init()
+clock = pygame.time.Clock()
 
 # define our grid
 TILE_SIZE = 64
 WINDOW_WIDTH = 10 * TILE_SIZE
 WINDOW_HEIGHT = 8 * TILE_SIZE 
 
-# 0 is grass, 1 is dirt 2 is sand
-grid = [
-    [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, ],
-    [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, ],
-    [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, ],
-    [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, ],
-    [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, ],
-    [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, ],
-    [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, ],
-    [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, ],
-]
 # define images for our background
 grass = pygame.image.load("images/Environment/grass.png")
 dirt = pygame.image.load("images/Environment/dirt.png")
@@ -33,19 +26,44 @@ tile_rect = grass.get_rect()
 # draw our screen with background
 screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 
-w_loc = 0
-h_loc = 0
+def draw_background():
+    # draw each tile onto our background
+    for r, grid_list in enumerate(grid):
+        for c, grid_element in enumerate(grid_list):
+            # blit the correc tile onto our screen
+            screen.blit(soils[grid_element], (c*TILE_SIZE, r*TILE_SIZE))
 
-# draw each tile onto our background
-for row in grid:
-    for i in row:
-        # blit the correc tile onto our screen
-        screen.blit(soils[i], (w_loc, h_loc))
-        w_loc += TILE_SIZE
-    h_loc += TILE_SIZE
-    w_loc = 0
+tank = Tank(screen)
 
 
+font = pygame.font.SysFont(None, 24)
+score = 1024
 
-pygame.display.flip()
-sleep(5)
+# while loop that runs the game
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        #check for keydowns
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                sys.exit()
+        if event.type == pygame.MOUSEMOTION:
+            # use  event.pos to aim the tank
+            tank.move(event.pos)
+
+    # use the clock to slow down FPS
+    clock.tick(60)
+    pygame.display.set_caption(f"FIDOH's Tank Game {clock.get_fps():.0f}")
+    # update the background
+    draw_background()
+    tank.draw()
+    # add in a score
+    img = font.render(f"Score: {score}", True, (255,0,0))
+    screen.blit(img, (20, 20))
+    score +=1
+
+    # last step to update our screen
+    pygame.display.flip()
+
+
--- tank.py
+++ tank.py
@@ -2,8 +2,26 @@ import pygame
 from pygame.sprite import Sprite
 
 class Tank(Sprite):
-    def __init__(self):
+    def __init__(self,screen):
+        self.image = pygame.image.load('images/Tanks/tankBlue_outline.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.x = 0
+        self.y = 0
+        self.screen = screen
+        self.h_border = self.screen.get_rect().centery
+        print(self.h_border)
+        print(self.screen.get_rect())
 
-    def move():
+    def move(self,location):
+        # get the x and y of passed location
+        loc_x, loc_y = location
+        # added feature, tank can't go beyond halfway point
+        if loc_y + self.rect.height//2 > self.h_border:
+            # border is violated, stop tank at border
+            self.rect.center = (loc_x,self.h_border-self.rect.height//2)
+        # move our rectangle to the passed in location
+        else:
+            self.rect.center = location
 
-    def draw():
+    def draw(self):
+        self.screen.blit(self.image,self.rect)
