e33510e18654e6b1c3a05a94d6ef68f65fc5dc53
Allan Elsberry <allanelsberry@Allans-Mac-mini.local>
2022-11-16 19:10:00 -0500

    finished keydown and refactored bg

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 from time import sleep
-from helper import grid
+from background import draw_background, TILE_SIZE
 import sys
 from tank import Tank
 
@@ -9,32 +9,15 @@ pygame.init()
 clock = pygame.time.Clock()
 
 # define our grid
-TILE_SIZE = 128
 WINDOW_WIDTH = 10 * TILE_SIZE
-WINDOW_HEIGHT = 8 * TILE_SIZE 
-
-# define images for our background
-grass = pygame.image.load("images/Retina/tileGrass1.png")
-dirt = pygame.image.load("images/Retina/tileSand1.png")
-sand = pygame.image.load("images/Retina/tileSand_roadNorth.png")
-
-soils = [grass,dirt,sand]
-
-# grab the dimension of our tile rectangle
-tile_rect = grass.get_rect()
+WINDOW_HEIGHT = 8 * TILE_SIZE
 
 # draw our screen with background
-screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-def draw_background():
-    # draw each tile onto our background
-    for r, grid_list in enumerate(grid):
-        for c, grid_element in enumerate(grid_list):
-            # blit the correc tile onto our screen
-            screen.blit(soils[grid_element], (c*TILE_SIZE, r*TILE_SIZE))
 
 tank = Tank(screen)
-
+bg = draw_background((WINDOW_WIDTH, WINDOW_HEIGHT))
 font = pygame.font.SysFont(None, 24)
 score = 1024
 
@@ -43,27 +26,29 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        #check for keydowns
+        # check for keydowns
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 sys.exit()
             if event.key == pygame.K_UP:
                 tank.change_speed(1)
+            if event.key == pygame.K_DOWN:
+                tank.change_speed(-1)
             if event.key == pygame.K_RIGHT:
                 tank.change_omega(1)
+            if event.key == pygame.K_LEFT:
+                tank.change_omega(-1)
 
     # use the clock to slow down FPS
     clock.tick(60)
     pygame.display.set_caption(f"FIDOH's Tank Game {clock.get_fps():.0f}")
     # update the background
-    draw_background()
+    screen.blit(bg, bg.get_rect())
     tank.draw()
     # add in a score
-    img = font.render(f"Score: {score}", True, (255,0,0))
+    img = font.render(f"Score: {score}", True, (255, 0, 0))
     screen.blit(img, (20, 20))
-    score +=1
+    score += 1
 
     # last step to update our screen
     pygame.display.flip()
-
-
